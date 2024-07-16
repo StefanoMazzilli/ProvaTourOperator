5f3cd489757c4931b1873106e8c2cc02f1a5107d
nicopadova <168413552+nicopadova@users.noreply.github.com>
2024-07-16 16:57:24 +0200

    2 versione
    ho aggiunto il get per EmailAndPassword

--- src/main/java/com/example/demo/repository/UtenteRepository.java
+++ src/main/java/com/example/demo/repository/UtenteRepository.java
@@ -1,5 +1,7 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import com.example.demo.model.Utente;
 
 @Repository
 public interface UtenteRepository extends JpaRepository<Utente, Long> {
+	List <Utente> findByEmailAndPassword(String email, String password);
 
 }
