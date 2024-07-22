14928e92997fbe78a54122163fd7019acd81866b
GabrielePusceddu <168413403+GabrielePusceddu@users.noreply.github.com>
2024-07-22 15:06:03 +0200

    Add files via upload

---
+++ src/main/java/com/example/demo/model/Contatto.java
@@ -0,0 +1,57 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Contatto {
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Integer getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(Integer telefono) {
+		this.telefono = telefono;
+	}
+	public String getCommento() {
+		return commento;
+	}
+	public void setCommento(String commento) {
+		this.commento = commento;
+	}
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String nome;
+    private String cognome;
+    private String email;
+    private Integer telefono;
+    private String commento;
+
+}
